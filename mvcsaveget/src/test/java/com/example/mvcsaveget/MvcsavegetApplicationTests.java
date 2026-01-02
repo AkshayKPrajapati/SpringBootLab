f3800edbeeadcfdba73656aeba595ec337a121e9
AkshayKPrajapati <akshayprajapati6200@outlook.com>
2026-01-02 18:58:10 +0530

    mvcsaveget

---
+++ mvcsaveget/src/test/java/com/example/mvcsaveget/MvcsavegetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcsaveget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcsavegetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
