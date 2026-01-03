29464b0ecd5e3df4391596a82e1477a6b2a7ec72
AkshayKPrajapati <akshayprajapati6200@outlook.com>
2026-01-03 23:34:56 +0530

    delete data api working

--- springbootcrudresponseentitiy/src/main/java/com/springbootcrudresponseentitiy/controller/StudentController.java
+++ springbootcrudresponseentitiy/src/main/java/com/springbootcrudresponseentitiy/controller/StudentController.java
@@ -1,5 +1,7 @@
 package com.springbootcrudresponseentitiy.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,7 +15,9 @@ import com.springbootcrudresponseentitiy.services.StudentService;
 import ch.qos.logback.core.joran.spi.HttpUtil.RequestMethod;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -47,5 +51,25 @@ public class StudentController {
 	public ResponseEntity<Iterable<Student>> fetchStudentDetails() {
 		Iterable<Student> students = studentService.getAllStudent();
 		return ResponseEntity.ok(students);
+	}
+	
+	@GetMapping("/getStudent/{id}")
+	public Optional<Student> getStudentById(@PathVariable int id) {
+		Optional<Student> student=this.studentService.StudentDetailsById(id);
+		System.out.println(student);
+		return student;
+	}
+	
+	@PostMapping("/getStudent")
+	public Optional<Student> StudentById(@RequestParam("id") int id) {
+		Optional<Student> student=this.studentService.StudentDetailsById(id);
+		System.out.println(student);
+		return student;
+	}
+	
+	@DeleteMapping("deleteStudentByid/{id}")
+	public String deleteStudent(@PathVariable int id ) {
+		this.studentService.deleteStudent(id);
+		return "Student with ID " + id + " has been deleted successfully";
 	}
 }
--- springbootcrudresponseentitiy/src/main/java/com/springbootcrudresponseentitiy/services/StudentService.java
+++ springbootcrudresponseentitiy/src/main/java/com/springbootcrudresponseentitiy/services/StudentService.java
@@ -1,5 +1,7 @@
 package com.springbootcrudresponseentitiy.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,16 +13,28 @@ public class StudentService {
 	@Autowired
 	StudentRepoistroy studentRepoistroy;
 	
-	
+	//insert new data 
 	public void saveStudentDetails(Student student) {
 		this.studentRepoistroy.save(student);
 		System.out.println("student details");
 	}
-	
+	// fetch all data
 	public Iterable<Student> getAllStudent() {
 		Iterable<Student> student = this.studentRepoistroy.findAll();
 		System.out.println("Student : "  +student);
 		return student;
 	}
 	
+	
+	// fetch single data
+	public Optional<Student> StudentDetailsById(int id) {
+		Optional<Student> student = this.studentRepoistroy.findById(id);
+		System.out.println("Student : "+student);
+		return student;
+	}
+	
+	//delete Student
+	public void deleteStudent(int id) {
+		 this.studentRepoistroy.deleteById(id);
+	}
 }
